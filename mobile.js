db5d636a6e2be439f0575687bba4da62a7130e00
spacecowboy888 <11juanson@gmail.com>
2026-02-13 16:43:04 +0800

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -2,81 +2,55 @@ let highestZ = 1;
 
 class Paper {
   holdingPaper = false;
-  touchStartX = 0;
-  touchStartY = 0;
-  touchMoveX = 0;
-  touchMoveY = 0;
   prevTouchX = 0;
   prevTouchY = 0;
-  velX = 0;
-  velY = 0;
-  rotation = Math.random() * 30 - 15;
   currentPaperX = 0;
   currentPaperY = 0;
-  rotating = false;
+  rotation = Math.random() * 30 - 15;
 
   init(paper) {
-    // Dragging logic
+    // Touch start
+    paper.addEventListener('touchstart', (e) => {
+      e.preventDefault();
+
+      this.holdingPaper = true;
+      highestZ += 1;
+      paper.style.zIndex = highestZ;
+
+      this.prevTouchX = e.touches[0].clientX;
+      this.prevTouchY = e.touches[0].clientY;
+    });
+
+    // Touch move
     paper.addEventListener('touchmove', (e) => {
       e.preventDefault();
 
       if (this.holdingPaper) {
-        this.touchMoveX = e.touches[0].clientX;
-        this.touchMoveY = e.touches[0].clientY;
+        const touchX = e.touches[0].clientX;
+        const touchY = e.touches[0].clientY;
 
-        this.velX = this.touchMoveX - this.prevTouchX;
-        this.velY = this.touchMoveY - this.prevTouchY;
+        const velX = touchX - this.prevTouchX;
+        const velY = touchY - this.prevTouchY;
 
-        this.currentPaperX += this.velX;
-        this.currentPaperY += this.velY;
+        this.currentPaperX += velX;
+        this.currentPaperY += velY;
 
-        this.prevTouchX = this.touchMoveX;
-        this.prevTouchY = this.touchMoveY;
+        this.prevTouchX = touchX;
+        this.prevTouchY = touchY;
 
         paper.style.transform =
           `translate(${this.currentPaperX}px, ${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
     });
 
-    // Touch start
-    paper.addEventListener('touchstart', (e) => {
-      e.preventDefault();
-
-      if (this.holdingPaper) return;
-      this.holdingPaper = true;
-
-      highestZ += 1;
-      paper.style.zIndex = highestZ;
-
-      this.touchStartX = e.touches[0].clientX;
-      this.touchStartY = e.touches[0].clientY;
-      this.prevTouchX = this.touchStartX;
-      this.prevTouchY = this.touchStartY;
-
-      // reset position kung bagong hawak
-      if (!this.currentPaperX) this.currentPaperX = 0;
-      if (!this.currentPaperY) this.currentPaperY = 0;
-    });
-
     // Touch end
     paper.addEventListener('touchend', () => {
       this.holdingPaper = false;
-      this.rotating = false;
-    });
-
-    // Optional: two-finger rotation
-    paper.addEventListener('gesturestart', (e) => {
-      e.preventDefault();
-      this.rotating = true;
-    });
-    paper.addEventListener('gestureend', () => {
-      this.rotating = false;
     });
   }
 }
 
 const papers = Array.from(document.querySelectorAll('.paper'));
-
 papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
